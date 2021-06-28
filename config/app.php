987a381233b4fd923d1c91ce589252bf418f60c7
Jaweed Khan <mr.jaweedkhan@gmail.com>
2021-06-28 21:54:07 +0530

    spatie permissions added

--- config/app.php
+++ config/app.php
@@ -174,6 +174,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /**
+         * spatie Package Provider
+         */
+        Spatie\Permission\PermissionServiceProvider::class,
 
     ],
 
